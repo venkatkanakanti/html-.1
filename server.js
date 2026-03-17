e26c8575ac2489d92d5b5bebc951aa1bfbd92698
venkatkanakanti <kanakantivenkataramanareddy90@gmail.com>
2026-03-17 12:16:56 +0530

    fixed port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // middleware
 app.use(bodyParser.urlencoded({ extended: true }));
